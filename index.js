bf0479e8a5c3248ca157d2d417d637e6816ebb14
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-25 10:25:35 -0500

    added different functions for practice, will be adding more practice later on

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 "use strict";
 
-console.log("Hello World!");
+// console.log("Hello World!");
 
-function counterPrinter (userInput){
-    for(let i = 0; i < userInput; i++){
-        console.log(`My new favorite number is ${i}.`);
-    }    
+//function counterPrinter (userInput){
+//    for(let i = 0; i < userInput; i++){
+//        console.log(`My new favorite number is ${i}.`);
+//    }    
+//}
+
+//counterPrinter(parseInt(prompt("Please enter a whole number between 1 and 1000! 🤡"))); 
+
+
+// function below
+function ageIsGreater(userInput){
+    if(userInput > 26){
+        return true;
+    }
+        return false
 }
+//invocation
+let isAgeGreater = ageIsGreater(
+    parseInt(
+        prompt("Let's see if you are older than I am!\nPlease enter your age (in whole numners)."
+        )
+    )
+);
 
-counterPrinter(parseInt(prompt("Please enter a whole number between 1 and 1000! 🤡")));
+if (isAgeGreater) {
+    alert("Congrats! You're old!")
+}else{
+    alert("Ha! I am older than you!")
+}
