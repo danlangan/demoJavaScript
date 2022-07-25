b2b52bb46d65a3d6910c9de6c0695abc76bde778
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-25 10:07:59 -0500

    created a function in index.js that goes over an integer that is user input, a basic for loop in pascal's demo video. I had to use a back tick for my log to the console to properly reference the integer and not have it print  to the console. Make note of that for later

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 "use strict";
 
-console.log("Hello World!")
+console.log("Hello World!");
+
+function counterPrinter (userInput){
+    for(let i = 0; i < userInput; i++){
+        console.log(`My new favorite number is ${i}.`);
+    }    
+}
+
+counterPrinter(parseInt(prompt("Please enter a whole number between 1 and 1000! 🤡")));
