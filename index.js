941f5377944eb66a26eb0d926e82e8b41773db2d
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-02 12:38:20 -0500

    went through the code demo with pascal and changed the html to do the demoing in the index.js folder and coded all the stuff in that folder so I could keep it separate from the other practice

--- index.js
+++ index.js
@@ -12,22 +12,112 @@
 
 
 // function below
-function ageIsGreater(userInput){
-    if(userInput > 26){
-        return true;
-    }
-        return false
-}
-//invocation
-let isAgeGreater = ageIsGreater(
-    parseInt(
-        prompt("Let's see if you are older than I am!\nPlease enter your age (in whole numners)."
-        )
-    )
-);
+//function ageIsGreater(userInput){
+   // if(userInput > 26){
+  //      return true;
+ //   }
+   //     return false
+//}
+//     //invocation
+//let isAgeGreater = ageIsGreater(
+   // parseInt(
+   //     prompt("Let's see if you are older than I am!\nPlease enter your age (in whole numners)."
+//        )
+ //   )
+//);
 
-if (isAgeGreater) {
-    alert("Congrats! You're old!")
-}else{
-    alert("Ha! I am older than you!")
+//if (isAgeGreater) {
+//    alert("Congrats! You're old!")
+//}else{
+//    alert("Ha! I am older than you!")
+//}
+
+// Collection of objects
+
+let driverArray = [
+    {
+        name: "Mario",
+        engineClass: 100,
+        favoriteTrack: "Piranha Plant Slide",
+        colorPalette: "red blue",
+    },
+    {
+        name: "Luigi",
+        engineClass: 100,
+        favoriteTrack: "Super Bell Subway",
+        colorPalette: "green blue",
+    },
+    {
+        name: "Yoshi",
+        engineClass: 50,
+        favoriteTrack: "Shy Guy Falls",
+        colorPalette: "green white",
+    },
+    {
+        name: "Toad",
+        engineClass: 50,
+        favoriteTrack: "Toad Harbor",
+        colorPalette: "red white",
+    },
+    {
+        name: "Bowser",
+        engineClass: 150,
+        favoriteTrack: "Neo Bowser City",
+        colorPalette: "yellow green",
+    },
+    {
+        name: "Wario",
+        engineClass: 150,
+        favoriteTrack: "Mount Wario",
+        colorPalette: "yellow purple",
+    },
+    {
+        name: "Waluigi",
+        engineClass: 100,
+        favoriteTrack: "Electrodome",
+        colorPalette: "purple black",
+    },
+    {
+        name: "DK",
+        engineClass: 150,
+        favoriteTrack: "Cloudtop Cruise",
+        colorPalette: "brown red",
+    },
+];
+
+
+function searchByColorPalette(array){
+    let userInput = prompt("Please enter a color to search by: ");
+    let foundDrivers = array.filter(function(driverElement){
+        if (driverElement.colorPalette.includes(userInput)) {
+            return true;
+        }
+    });
+    return foundDrivers;
 }
+
+
+function searchUserDefinedProp (array){
+    let userInputProp = prompt("Enter the property that you would like to search by: ");
+    let userInoutVal = prompt("Enter the value of that property: ");
+    let foundItems = array.filter(function(element){
+        try {
+            if(element[userInputProp].includes(userInoutVal)){
+                return true;    
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            if(element[userInputProp]===parseInt(userInoutVal)){
+                return true;
+            }
+        }
+    });
+    return foundItems;
+}
+let driverPrinter = searchUserDefinedProp(driverArray).map(function(element){
+    return `${element.name} likes to driver their ${element.engineClass}cc kart around their favorite track, ${element.favoriteTrack}!`
+});
+
+console.log(driverPrinter);
